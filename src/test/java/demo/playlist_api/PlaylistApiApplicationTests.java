17364ef2abbea202381c9da190d0f1888b2269b5
danieleduardc <danielepuertah@gmail.com>
2025-09-02 16:12:40 -0500

    primer commit con dependecias base y configuracion

---
+++ src/test/java/demo/playlist_api/PlaylistApiApplicationTests.java
@@ -0,0 +1,13 @@
+package demo.playlist_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaylistApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
